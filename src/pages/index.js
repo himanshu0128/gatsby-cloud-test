1dfe90e8753ca68080eb7d7362343dac9bf69d17
Himanshu Gupta <mr.hemangpta@gmail.com>
2020-07-08 18:22:12 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <p>What a world.</p>
+    </div>
+  )
 }
